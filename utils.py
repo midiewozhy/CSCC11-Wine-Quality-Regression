da9d9774f19731cca96ecf58c9e71843f037eae6
MZ <marshal.hy.zhu@gmail.com>
2026-03-19 09:38:13 -0400

    save local work before pull

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import csv
 import numpy as np
 import pandas as pd
@@ -83,3 +84,90 @@ def preprocessing(red_file, white_file, output_file=None, test_size = 0.2, rd_st
 
 
 
+=======
+import csv
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Merge the dataset into one and add an indicator to show if it is red or white wine
+red_file = 'winequality-red.csv'
+white_file = 'winequality-white.csv'
+output_file = 'winequality-modified.csv'
+
+def preprocessing(red_file, white_file, output_file=None, test_size = 0.2, rd_state=42):
+    """
+    Args: 
+    red_file: the name of data containing info of red wine
+    white_file: the name of data containing info of white wine
+    output_file: the name of file that you want to save the final concatenated data
+    test_size: the ratio of test data
+    rd_state: random seed for reproductibility
+    output:
+    X_train: -
+    y_train: -
+    X_test: -
+    y_test -
+    """
+    final_data = []
+    # read the data file
+    for file in (red_file, white_file):
+        with open(file, mode='r', newline='') as infile:
+            reader = csv.reader(infile, delimiter=';')
+            rows = list(reader)
+
+        header = rows[0]
+        header.insert(-1, 'red')
+
+        is_red = 1 if file==red_file else 0
+
+        for row in rows[1:]:
+            row.insert(-1, is_red)
+
+        # only include one header row
+        if is_red == 1:
+            final_data.extend(rows)
+        else:
+            final_data.extend(rows[1:])
+
+    if output_file != None:
+        with open(output_file, mode='', newline='') as outfile:
+            writer = csv.writer(outfile, delimiter=';')
+            writer.writerows(final_data)
+
+    # turn the list into a datafram
+    headers = final_data[0]
+    values = final_data[1:]
+    data = pd.DataFrame(values, columns=headers)
+    data = data.apply(pd.to_numeric)
+
+    # Basic data preprocessing and summary
+    summary = data.describe()
+    print(summary)
+
+    # Check for missing data
+    print(data.isnull().sum())
+
+    # Check for the share of red and white wine
+    print(data['red'].value_counts())
+
+    # Shuffule the data frac=1 means all shuffled, rd_state for reproductibility, reset index true to reassign the index
+    data_shuffled = data.sample(frac=1, random_state=rd_state).reset_index(drop=True)
+
+    # Train test split
+    train_data, test_data = train_test_split(data_shuffled, test_size=test_size, random_state=rd_state)
+    X_train = train_data.iloc[:,0:12]
+    y_train = train_data.iloc[:,12]
+    X_test = test_data.iloc[:,0:12]
+    y_test = test_data.iloc[:,12]
+
+    return X_train, y_train, X_test, y_test
+
+
+
+
+
+
+
+
+>>>>>>> 6d95979 (save local work before pull)
